2683f210d851492d7699a7718e1384cda1195c64
codedump00 <blazinasis@gmail.com>
2019-04-30 11:56:07 +0545

    added google maps feature

--- lib/main.dart
+++ lib/main.dart
@@ -22,21 +22,21 @@
 
 import 'dart:async';
 
-import 'package:location/location.dart';
+// import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-var location = new Location();
+// var location = new Location();
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    location.onLocationChanged().listen((LocationData currentLocation) {
-      print(currentLocation.latitude);
-      print(currentLocation.longitude);
-    });
+    // location.onLocationChanged().listen((LocationData currentLocation) {
+    //   print(currentLocation.latitude);
+    //   print(currentLocation.longitude);
+    // });
     return MaterialApp(
       title: 'Flutter Google Maps Demo',
       home: MapSample(),
